0c184ebbda120532de198297bc6ab11e00e853c3
Laila Campbell <lailacampbell96@gmail.com>
2020-03-17 00:05:19 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,5 @@
 class Chatty_Student < Student 
-  
-  def hello
+   def hello
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
@@ -9,5 +8,4 @@ class Chatty_Student < Student
         super 
     end 
   end 
-  
 end 
